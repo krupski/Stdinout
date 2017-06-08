134d2bff2600d6816f07a2d03b1aa354b473d8fa
Roger A. Krupski <rakrupski@verizon.net>
2017-06-08 15:08:43 -0400

    Updated

--- src/Stdinout.cpp
+++ src/Stdinout.cpp
@@ -1,9 +1,9 @@
 //////////////////////////////////////////////////////////////////////////////
 //
 //  Stdinout.cpp - connect various character devices to standard streams
-//  Copyright (c) 2014, 2016 Roger A. Krupski <rakrupski@verizon.net>
+//  Copyright (c) 2014, 2017 Roger A. Krupski <rakrupski@verizon.net>
 //
-//  Last update: 03 August 2016
+//  Last update: 07 June 2017
 //
 //  This library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -20,16 +20,12 @@
 //
 //////////////////////////////////////////////////////////////////////////////
 
-#include "Stdinout.h"
+#include <Stdinout.h>
 
-static Print *stream_ptr0 = NULL; // stdin stream pointer
-static Print *stream_ptr1 = NULL; // stdout stream pointer
-static Print *stream_ptr2 = NULL; // stderr stream pointer
-
-// connect stdio to all devices
-void STDINOUT::open (Print &iostr)
+// connect stdin, stdout and stderr to same device
+void STDINOUT::open (Print &iostream)
 {
-	open (iostr, iostr, iostr);
+	open (iostream, iostream, iostream);
 }
 
 // connect stdin to input device, stdout and stderr to output device
@@ -43,102 +39,80 @@ void STDINOUT::open (Print &inpstr, Print &outstr, Print &errstr)
 {
 	close();  // close any that may be open
 
-	if (stdin == NULL) { // open stdin
-		stdin = fdevopen (NULL, getchar0);
-		stream_ptr0 = &inpstr;
-	}
+	stdin = fdevopen (NULL, _getchar0);
+	_stream_ptr0 = (Stream *)(&inpstr);
 
-	if (stdout == NULL) { // open stdout
-		stdout = fdevopen (putchar1, NULL);
-		stream_ptr1 = &outstr;
-	}
+	stdout = fdevopen (_putchar1, NULL);
+	_stream_ptr1 = &outstr;
 
-	if (stderr == NULL) { // open stderr
-		stderr = fdevopen (putchar2, NULL);
-		stream_ptr2 = &errstr;
-	}
+	stderr = fdevopen (_putchar2, NULL);
+	_stream_ptr2 = &errstr;
 }
 
 // disconnect stdio from stream(s)
 void STDINOUT::close (void)
 {
-	if (stderr) { // close stderr
-		fclose (stderr);
-		stderr = NULL;
-		stream_ptr2 = NULL;
-	}
+	fclose (stdin);
+	stdin = NULL;
+	_stream_ptr0 = NULL;
 
-	if (stdout) { // close stdout
-		fclose (stdout);
-		stdout = NULL;
-		stream_ptr1 = NULL;
-	}
+	fclose (stdout);
+	stdout = NULL;
+	_stream_ptr1 = NULL;
 
-	if (stdin) { // close stdin
-		fclose (stdin);
-		stdin = NULL;
-		stream_ptr0 = NULL;
-	}
+	fclose (stderr);
+	stderr = NULL;
+	_stream_ptr2 = NULL;
 }
 
 // return stream connected to FILE (i.e. stdin, stdout or stderr).
 Print &STDINOUT::getStream (FILE *fp)
 {
 	FILE *f[] = { NULL, stdin, stdout, stderr }; // file pointers
-	Print *s[] = { NULL, stream_ptr0, stream_ptr1, stream_ptr2 }; // stream pointers
-	int8_t x = (sizeof (f) / sizeof (*f)); // pointer count
+	Print *s[] = { NULL, _stream_ptr0, _stream_ptr1, _stream_ptr2 }; // stream pointers
+	int8_t x = _array_size(f); // pointer count
 
 	while (x--) { // scan through them
 		if (f[x] == fp) { // if stdio matches...
 			break; // ...return it's pointer
 		}
 	}
-	if (x < 0) {
-		x = 0; // found nothing, return NULL
+
+	if (x < 0) { // x == -1 if search fails
+		x = 0; // found nothing, return s[0] (NULL)
 	}
+
 	return *s[x]; // if the loop finishes, return null
 }
 
 // Function that fgetc, fread, scanf and related
 // will use to read a char from stdin
-int STDINOUT::getchar0 (FILE *fp)
+int STDINOUT::_getchar0 (FILE *fp)
 {
-	if (fp == stdin) {
-		while (! (stream_ptr0->available()));  // wait until a character is available...
-		return (stream_ptr0->read());  // ...then grab it and return
-	} else {
-		return _FDEV_ERR;
-	}
+	while (! (_stream_ptr0->available()));  // wait until a character is available...
+	return (_stream_ptr0->read());  // ...then grab it and return
 }
 
 // function that printf and related will use to write
 // a char to stdout and auto-add a CR to a LF
-int STDINOUT::putchar1 (char c, FILE *fp)
+int STDINOUT::_putchar1 (char c, FILE *fp)
 {
-	if (fp == stdout) {
-		if (c == '\n') { // if linefeed
-			stream_ptr1->write ((char) '\r'); // also send CR
-		}
-		stream_ptr1->write ((char) c);
-		return 0;
-	} else {
-		return _FDEV_ERR;
+	if (c == '\n') { // if linefeed
+		_stream_ptr1->write ((char) '\r'); // also send CR
 	}
+	_stream_ptr1->write ((char) c);
+	return 0;
 }
 
 // function that printf and related will use to write
-// a char to stdout and auto-add a CR to a LF
-int STDINOUT::putchar2 (char c, FILE *fp)
+// a char to stderr and auto-add a CR to a LF
+int STDINOUT::_putchar2 (char c, FILE *fp)
 {
-	if (fp == stderr) {
-		if (c == '\n') { // if linefeed
-			stream_ptr2->write ((char) '\r'); // also send CR
-		}
-		stream_ptr2->write ((char) c);
-		return 0;
-	} else {
-		return _FDEV_ERR;
+	if (c == '\n') { // if linefeed
+		_stream_ptr2->write ((char) '\r'); // also send CR
 	}
+	_stream_ptr2->write ((char) c);
+	return 0;
 }
 
 STDINOUT STDIO; // Preinstantiate STDIO object
--- src/Stdinout.h
+++ src/Stdinout.h
@@ -1,9 +1,9 @@
 //////////////////////////////////////////////////////////////////////////////
 //
 //  Stdinout.h - connect various character devices to standard streams
-//  Copyright (c) 2014, 2016 Roger A. Krupski <rakrupski@verizon.net>
+//  Copyright (c) 2014, 2017 Roger A. Krupski <rakrupski@verizon.net>
 //
-//  Last update: 03 August 2016
+//  Last update: 07 June 2017
 //
 //  This library is free software: you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
@@ -23,26 +23,33 @@
 #ifndef STD_IN_OUT_H
 #define STD_IN_OUT_H
 
-#include <stdio.h>
-#include "Print.h"
+#include <Stream.h>
 
-class STDINOUT {
+static Stream *_stream_ptr0 = NULL; // stdin stream pointer (using Stream)
+static Print  *_stream_ptr1 = NULL; // stdout stream pointer (using Print)
+static Print  *_stream_ptr2 = NULL; // stderr stream pointer (using Print)
 
-	public:
+class STDINOUT
+{
+	public: // main functions
 		void open (Print &);
 		void open (Print &, Print &);
 		void open (Print &, Print &, Print &);
 		void close (void);
 		Print &getStream (FILE *);
-
-	private:
-		static int getchar0 (FILE *); // char read for stdin
-		static int putchar1 (char, FILE *); // char write for stdout
-		static int putchar2 (char, FILE *); // char write for stderr
+	private: // internal functions
+		#define _array_size(a)(sizeof(a)/sizeof(*a))
+		static int _getchar0 (FILE *); // char read for stdin
+		static int _putchar1 (char, FILE *); // char write for stdout
+		static int _putchar2 (char, FILE *); // char write for stderr
+	public: // virtual functions
+		virtual size_t write (uint8_t) { return 0; }
+		virtual int available (void) { return 0; }
+		virtual int read (void) { return 0; }
 };
 
-extern STDINOUT STDIO; // Preinstantiate STDIO object
+extern STDINOUT STDIO; // Expose STDIO object
 
-#endif
+#endif // #ifndef STD_IN_OUT_H
 
 // end of Stdinout.h
